3f5793dd2c1b1f2a1aa75d9a4fde24cc2a39a068
imightexist <94249457+imightexist@users.noreply.github.com>
2024-07-31 18:03:06 -0500

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -5,4 +5,11 @@ npm i
 echo installing mesa3d
 pacman -Sy mesa
 chmod +x download.sh
+echo installing 7zip
+pacman -Sy p7zip-full
+echo setting up dirs
+mkdir assets
+mkdir data
+mkdir json
+mkdir versions
 ./download.sh
