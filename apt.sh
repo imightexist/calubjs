27511312f10cdbb0bfd6e40f23922a8ee1933e5b
imightexist <94249457+imightexist@users.noreply.github.com>
2024-07-31 18:02:41 -0500

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -6,5 +6,12 @@ echo installing mesa3d
 sudo add-apt-repository ppa:kisak/kisak-mesa
 sudo apt update
 sudo apt upgrade
+echo installing 7zip
+sudo apt install p7zip-full
+echo setting up dirs
+mkdir assets
+mkdir data
+mkdir json
+mkdir versions
 chmod +x download.sh
 ./download.sh
