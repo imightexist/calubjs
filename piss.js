f9def37e3181227515d16cc2b5f402229163acf4
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-01 12:01:31 -0500

    Update piss.js

--- piss.js
+++ piss.js
@@ -231,9 +231,13 @@ wget({url:'https://piston-meta.mojang.com/mc/game/version_manifest_v2.json',dest
                                                             if (Object.keys(shit.libraries[i]).includes("rules")){
                                                                 if (Object.keys(shit.libraries[i].rules[0]).includes("os")){
                                                                     if (shit.libraries[i].rules[0].os.name == "windows" && shit.libraries[i].name.endsWith("windows")){
+                                                                        let semen = shit.libraries[i].downloads.artifact.url.split("/")[shit.libraries[i].downloads.artifact.url.split("/").length-1]
                                                                         console.log("downloading jar (" + (i + 1) + "/" + shit.libraries.length + "): " + shit.libraries[i].downloads.artifact.url.split("/")[shit.libraries[i].downloads.artifact.url.split("/").length-1])
                                                                         download = proc.spawn('aria2c', ['-x16', '-s16', '-m16', shit.libraries[i].downloads.artifact.url, '--dir=versions/' + res2.version], { shell: true, detached: true })
                                                                         download.on('close', function (c3) {
+                                                                            if (shit.libraries[i].name.endsWith("natives-windows"){
+                                                                                proc.spawn('7z',['x','-aoa','versions/'+res2.version+'/'+semen,'-oversions/'+res2.version+'/natives'],{shell:true,detached:true})
+                                                                            }
                                                                             downloadLib(shit.libraries, ++i)
                                                                         })
                                                                     }else{
