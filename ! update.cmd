1bed4b82d9f539ff06a9ba16369727acc0f8ec9e
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-03 18:47:55 -0500

    Create ! update.cmd

---
+++ ! update.cmd
@@ -0,0 +1,8 @@
+@prompt $G
+for %I in (.) do set pwd=%~nxI
+cd..
+"%pwd%\aria2c" -x16 -m16 -s16 https://github.com/imightexist/calubjs/archive/refs/heads/main.zip --out=calub-main.zip
+rename %pwd% calub-main
+"calub-main\7z" x -aoa calub-main.zip
+cd calub-main
+"! install.cmd"
