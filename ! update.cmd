0e73264537f23ab6c3f44d7b26fee128d3d8f4a8
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-03 18:57:39 -0500

    Update ! update.cmd

--- ! update.cmd
+++ ! update.cmd
@@ -1,8 +1,9 @@
 @prompt $G
 for %%I in (.) do set pwd=%%~nxI
 cd..
-"%pwd%\aria2c" -x16 -m16 -s16 https://github.com/imightexist/calubjs/archive/refs/heads/main.zip --out=calub-main.zip
-rename %pwd% calub-main
-"calub-main\7z" x -aoa calub-main.zip
-cd calub-main
+del /q calubjs-main.zip
+"%pwd%\aria2c" -x16 -m16 -s16 https://github.com/imightexist/calubjs/archive/refs/heads/main.zip --out=calubjs-main.zip
+rename %pwd% calubjs-main
+"calubjs-main\7z" x -aoa calubjs-main.zip
+cd calubjs-main
 "! install.cmd"
