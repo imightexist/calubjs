49937dac55edac080261b79accac16280188e11d
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-03 17:54:17 -0500

    Create ! install.cmd

---
+++ ! install.cmd
@@ -0,0 +1,31 @@
+@prompt $G
+::@echo 64 bit only
+@pushd %~dp0%
+@echo downloading calubcraft
+::aria2c "https://raw.githubusercontent.com/imightexist/calubjs/main/! username.cmd"
+del "! download.cmd" /q
+aria2c "https://raw.githubusercontent.com/imightexist/calubjs/main/! download.cmd"
+del piss.js /q
+aria2c "https://raw.githubusercontent.com/imightexist/calubjs/main/piss.js"
+@echo.
+@echo downloading nodejs
+aria2c -x16 -m16 -s16 https://nodejs.org/download/release/v22.5.1/node-v22.5.1-win-x64.7z
+@echo.
+@echo extracting nodejs
+7z x node-v22.5.1-win-x64.7z -onode
+del /q node-v21.5.0-win-x86.7z
+@echo.
+@echo installing packages
+del package.json /q
+aria2c https://raw.githubusercontent.com/imightexist/calubjs/main/package.json
+::cmd /c "node\npm" i prompt
+::cmd /c "node\npm" i node-wget
+@echo.
+@echo run collabvm.cmd as admin if you are running this on collabvm before continuing
+@pause
+cmd /c "node\npm" config set strict-ssl false
+cmd /c "node\npm" i
+:: @echo wowza you arent a normie running this on a 32 bit vm
+::cmd /c "! username.cmd"
+@prompt
+cmd /c "! download.cmd"
