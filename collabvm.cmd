0ecc607a462a9c94f6f00c6b0345059d309dd942
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-03 18:48:49 -0500

    Update collabvm.cmd

--- collabvm.cmd
+++ collabvm.cmd
@@ -6,7 +6,7 @@ takeown /f C:\windows\system32\opengl32.dll
 icacls C:\windows\system32\opengl32.dll /grant Administrators:F
 icacls C:\windows\syswow64\opengl32.dll /grant Administrators:F
 aria2c -x16 -m16 -s16 https://github.com/imightexist/calubjs/raw/mesa/mesa.7z
-7z x mesa.7z -omesa
+7z x -aos mesa.7z -omesa
 del mesa.7z
 copy /y mesa\gl32.dll C:\windows\syswow64\opengl32.dll
 copy /y mesa\gl64.dll C:\windows\system32\opengl32.dll
