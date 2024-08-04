137ae0d876575333f1bef283c6f5b7c14a184a00
Nikitas <93310518+zanikitas@users.noreply.github.com>
2024-08-04 07:10:29 +0700

    silent

--- pacman.sh
+++ pacman.sh
@@ -1,12 +1,12 @@
 echo installing node.js packages
-pacman -Sy nodejs npm wget --noconfirm
+pacman -Sy nodejs npm wget --noconfirm > /dev/null
 npm config set strict-ssl false
 npm i
 echo installing mesa3d
-pacman -Sy mesa --noconfirm
+pacman -Sy mesa --noconfirm > /dev/null
 chmod +x download.sh
 echo installing 7zip
-pacman -Sy p7zip --noconfirm
+pacman -Sy p7zip --noconfirm > /dev/null
 echo setting up dirs
 mkdir assets
 mkdir data
