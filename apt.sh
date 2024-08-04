72221d03abb15f4b661f5766dd5800c46a3c448b
Nikitas <93310518+zanikitas@users.noreply.github.com>
2024-08-04 07:21:19 +0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,13 +1,13 @@
 echo installing node.js packages
-sudo apt install nodejs npm
+sudo apt install nodejs npm -y > /dev/null
 npm config set strict-ssl false
 npm i
 echo installing mesa3d
-sudo add-apt-repository ppa:kisak/kisak-mesa
-sudo apt update
-sudo apt upgrade
+sudo add-apt-repository ppa:kisak/kisak-mesa > /dev/null
+sudo apt update > /dev/null
+# sudo apt upgrade
 echo installing 7zip
-sudo apt install p7zip-full
+sudo apt install p7zip-full -y > /dev/null
 echo setting up dirs
 mkdir assets
 mkdir data
