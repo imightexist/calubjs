e89f02a1e7085c8b2234c5d9e742d0a2534ab81a
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-03 19:12:39 -0500

    Update ! update.cmd

--- ! update.cmd
+++ ! update.cmd
@@ -4,8 +4,7 @@ cd..
 del /q calubjs-main.zip
 "%pwd%\aria2c" -x16 -m16 -s16 https://github.com/imightexist/calubjs/archive/refs/heads/main.zip --out=calubjs-main.zip
 echo rename %pwd% calubjs-main > f.cmd
-echo "calubjs-main\7z" x -aoa >> f.cmd
-echo calubjs-main.zip >> f.cmd
+echo "calubjs-main\7z" x -aoa calubjs-main.zip >> f.cmd
 echo cd calubjs-main >> f.cmd
 echo "! install.cmd" >> f.cmd
 echo del "..\f.cmd" >> f.cmd
