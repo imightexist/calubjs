17f1809c172e1ccffcdbdbcbdbb5f14ce3494053
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-07 15:18:24 -0500

    node 15.8 moment

--- ! install.cmd
+++ ! install.cmd
@@ -22,12 +22,14 @@ echo notch > username.txt
 :: aria2c "https://raw.githubusercontent.com/imightexist/calubjs/main/piss.js"
 @echo.
 @echo downloading nodejs
-del node-v22.5.1-win-x64.7z /q
-aria2c -x16 -m16 -s16 https://nodejs.org/download/release/v22.5.1/node-v22.5.1-win-x64.7z
+::del node-v22.5.1-win-x64.7z /q
+::aria2c -x16 -m16 -s16 https://nodejs.org/download/release/v22.5.1/node-v22.5.1-win-x64.7z
+del node-v15.8.0-win-x64.7z /q
+aria2c -x16 -m16 -s16 https://nodejs.org/dist/v15.8.0/node-v15.8.0-win-x64.7z
 @echo.
 @echo extracting nodejs
-7z x -aoa node-v22.5.1-win-x64.7z
-del /q node-v22.5.1-win-x64.7z
+7z x -aoa node-v15.8.0-win-x64.7z
+del /q node-v15.8.0-win-x64.7z
 @echo.
 @echo installing packages
 del package.json /q
@@ -38,8 +40,8 @@ aria2c https://raw.githubusercontent.com/imightexist/calubjs/main/package.json
 @echo run collabvm.cmd as admin if you are running this on collabvm before continuing
 @pause
 set NODE_SKIP_PLATFORM_CHECK=1
-cmd /c "node-v22.5.1-win-x64\npm" config set strict-ssl false
-cmd /c "node-v22.5.1-win-x64\npm" i
+cmd /c "node-v15.8.0-win-x64\npm" config set strict-ssl false
+cmd /c "node-v15.8.0-win-x64\npm" i
 :: @echo wowza you arent a normie running this on a 32 bit vm
 ::cmd /c "! username.cmd"
 @prompt
