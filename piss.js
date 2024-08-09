28b70165f063567ae0cf967aa2d428912294d0b6
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-09 17:02:15 -0500

    Update piss.js

--- piss.js
+++ piss.js
@@ -117,7 +117,7 @@ wget({url:'https://piston-meta.mojang.com/mc/game/version_manifest_v2.json',dest
                                             //console.log("generating launch script (2/2)")
                                             mainClass = shit.mainClass
                                             //console.log("ree")
-                                            fs.writeFileSync('versions/' + res2.version + '/! run.sh', java + args, { flag: 'a' })
+                                            fs.writeFileSync('versions/' + res2.version + '/! run.sh', 'java' + args, { flag: 'a' })
                                             function downloadLib(list, i) {
                                                 //console.log(i + " " + shit.libraries.length)
                                                 if (shit.libraries.length == i) {
