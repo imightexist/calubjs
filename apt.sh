134d7a4071a93d193d2d393060bb5b3078773066
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-10 17:26:16 -0500

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,15 +1,15 @@
 mkdir auth
 mkdir assets
-mkdir assets\indexes
-mkdir assets\objects
+mkdir assets/indexes
+mkdir assets/objects
 mkdir data
 mkdir json
 mkdir versions
-echo notch > auth\username.txt
-echo notch > auth\uuid.txt
-echo notch > auth\token.txt
-echo 0 > auth\expire.txt
-echo true > auth\expired.txt
+echo notch > auth/username.txt
+echo notch > auth/uuid.txt
+echo notch > auth/token.txt
+echo 0 > auth/expire.txt
+echo true > auth/expired.txt
 sudo add-apt-repository ppa:kisak/kisak-mesa
 sudo apt update
 sudo apt install nodejs npm openjdk-21-jdk p7zip-full aria2 -y
