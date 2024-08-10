44a46f1f6ee95cd542d1082a397babd5e73e2e72
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-09 21:14:20 -0500

    automatic login coming soon

--- shid.js
+++ shid.js
@@ -1,3 +1,4 @@
 let auth = require('prismarine-auth')
 let flow = new auth.Authflow('calubjs','json',{authTitle:auth.Titles.MinecraftJava,deviceType:"Win32",flow:"sisu"})
-flow.getMinecraftJavaToken().then(console.log)
+flow.getMinecraftJavaToken({fetchProfile:true}).then(console.log)
+//flow.getXboxToken().then(console.log)
