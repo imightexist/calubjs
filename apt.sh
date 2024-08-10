537598a6ef54156055be2bbe6e7642f6b927b754
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-10 17:22:56 -0500

    Update and rename ! install.cmd to apt.sh

---
+++ apt.sh
@@ -0,0 +1,18 @@
+mkdir auth
+mkdir assets
+mkdir assets\indexes
+mkdir assets\objects
+mkdir data
+mkdir json
+mkdir versions
+echo notch > auth\username.txt
+echo notch > auth\uuid.txt
+echo notch > auth\token.txt
+echo 0 > auth\expire.txt
+echo true > auth\expired.txt
+sudo add-apt-repository ppa:kisak/kisak-mesa
+sudo apt update
+sudo apt install nodejs npm jdk-21-openjdk p7zip-full aria2 -y
+npm config set strict-ssl false
+npm i
+chmod +x download.sh
