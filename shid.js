aa7e3ca28d383afcf7926803fd371845fb304990
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-09 19:53:09 -0500

    Update shid.js

--- shid.js
+++ shid.js
@@ -1,3 +1,3 @@
 let auth = require('prismarine-auth')
-let flow = new auth.Authflow(String(Math.random()),undefined,{authTitle:auth.Titles.MinecraftJava})
+let flow = new auth.Authflow(String(Math.random()),undefined,{authTitle:auth.Titles.MinecraftJava,device:"Win32"})
 flow.getMinecraftJavaToken().then(console.log)
