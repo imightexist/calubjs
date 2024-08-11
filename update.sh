4cef6cc9d3da251bb058da8f508873a4aedad3ee
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-11 10:49:55 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,6 @@
-
+p=$(basename $PWD)
+cd ..
+rm -f calubjs-linucks.zip
+aria2c -x16 -m16 -s16 https://github.com/imightexist/calubjs/archive/refs/heads/linucks.zip --out=calubjs-linucks.zip
+rename $p calubjs-linucks
+7z x -aoa calubjs-linucks.zip
