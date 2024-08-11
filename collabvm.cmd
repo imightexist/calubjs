37f96b7e428890e05db6a8ba36c7133e002691a4
imightexist <94249457+imightexist@users.noreply.github.com>
2024-08-11 10:14:58 -0500

    Update collabvm.cmd

--- collabvm.cmd
+++ collabvm.cmd
@@ -2,12 +2,12 @@ pushd %~dp0%
 set NODE_SKIP_PLATFORM_CHECK=1
 aria2c http://router.collabnet.local/penis.crt
 cmd /c "node-v15.8.0-win-x64\npm" config set cafile "%cd%\penis.crt"
-takeown /f C:\windows\syswow64\opengl32.dll
-takeown /f C:\windows\system32\opengl32.dll
-icacls C:\windows\system32\opengl32.dll /grant Administrators:F
-icacls C:\windows\syswow64\opengl32.dll /grant Administrators:F
+takeown /f %systemroot%\syswow64\opengl32.dll
+takeown /f %systemroot%\system32\opengl32.dll
+icacls %systemroot%\system32\opengl32.dll /grant Administrators:F
+icacls %systemroot%\syswow64\opengl32.dll /grant Administrators:F
 aria2c -x16 -m16 -s16 https://github.com/imightexist/calubjs/raw/mesa/mesa.7z
 7z x -aos mesa.7z -omesa
 del mesa.7z
-copy /y mesa\gl32.dll C:\windows\syswow64\opengl32.dll
-copy /y mesa\gl64.dll C:\windows\system32\opengl32.dll
+copy /y mesa\gl32.dll %systemroot%\syswow64\opengl32.dll
+copy /y mesa\gl64.dll %systemroot%\system32\opengl32.dll
