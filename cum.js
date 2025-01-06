ebadf29d1b02a206edadb74be84ac2c05ef50e18
imightexist <94249457+imightexist@users.noreply.github.com>
2025-01-06 17:13:35 -0600

    Update cum.js

--- cum.js
+++ cum.js
@@ -1,1 +1,22 @@
+let fs = require('fs')
+let b = process.argv[2]
+let e = process.argv[3]
+let a = fs.readFileSync('versions/'+b+'/! run.cmd')
+let c = JSON.parse(fs.readFileSync('optifine/versions/'+b+'/'+b+'.json'))
+//let d = a.substring(a.lastIndexOf("\n"))
+let f = JSON.parse(fs.readFileSync('json'/+e+'.json'))
+//a = a.substring(0,a.lastIndexOf("\n"))
+a = a.replaceAll(e,b)
+if (Object.keys(c).includes("arguments")){
+  if (Object.keys(c.arguments).includes("game")){
+    c.arguments.game.forEach(function(g){
+      a += " "+g
+    })
+  }
+}
+if (Object.keys(c).includes("mainClass")){
+  a = a.replaceAll(f.mainClass,c.mainClass)
+}
 
+fs.unlinkSync('versions/'+b+'/! run.cmd')
+fs.writeFileSync('versions/'+b+'/! run.cmd',a)
