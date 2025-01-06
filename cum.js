094514ce1ec6c3494f65a40185dadd617b612a9c
imightexist <94249457+imightexist@users.noreply.github.com>
2025-01-06 17:29:23 -0600

    Update cum.js

--- cum.js
+++ cum.js
@@ -4,7 +4,7 @@ let e = process.argv[3]
 let a = fs.readFileSync('versions/'+b+'/! run.cmd')
 let c = JSON.parse(fs.readFileSync('optifine/versions/'+b+'/'+b+'.json'))
 //let d = a.substring(a.lastIndexOf("\n"))
-let f = JSON.parse(fs.readFileSync('json'/+e+'.json'))
+let f = JSON.parse(fs.readFileSync('json/'+e+'.json'))
 //a = a.substring(0,a.lastIndexOf("\n"))
 a = a.replaceAll(e,b)
 if (Object.keys(c).includes("arguments")){
