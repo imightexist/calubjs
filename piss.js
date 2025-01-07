8575ed4d5cc759908057f3ea2bbcecf64551f24b
imightexist <94249457+imightexist@users.noreply.github.com>
2025-01-07 11:58:43 -0600

    Update piss.js

--- piss.js
+++ piss.js
@@ -86,11 +86,11 @@ wget({url:'https://piston-meta.mojang.com/mc/game/version_manifest_v2.json',dest
                         shit.downloads = {client:{url:"https://archive.org/download/Minecraft-JE-Classic/"+version[0].id+"/"+version[0].id+".jar"}}
                         shit.assetIndex = {"id": "pre-1.6", "sha1": "3d8e55480977e32acd9844e545177e69a52f594b", "size": 74091, "totalSize": 49505710, "url": "https://launchermeta.mojang.com/v1/packages/3d8e55480977e32acd9844e545177e69a52f594b/pre-1.6.json"}
                     }
-                    shit.libraries.forEach(function(f){
-                        if (Object.keys(f).includes("downloads")){
-                            f.downloads = {artifact:{url:"https://"+f.name.replaceAll(".","/").replace(":","/").replaceAll(":","-")+".jar"}}
+                    for (let i = 0; i < shit.libraries.length; i++){
+                        if (Object.keys(shit.libraries[i]).includes("downloads")){
+                            shit.libraries[i].downloads = {artifact:{url:"https://"+shit.libraries[i].name.replaceAll(".","/").replace(":","/").replaceAll(":","-")+".jar"}}
                         }
-                    })
+                    }
                     let clientDL = proc.spawn('aria2c', ['-x16', '-s16', '-m16', shit.downloads.client.url, '--dir="versions/' + res2.version + '"'], { shell: true, detached: true })
                     clientDL.on('close', function (c2) {
                         let javaDL, javaZIP;
